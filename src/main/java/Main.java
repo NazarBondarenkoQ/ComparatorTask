9b7b5695a1a39b6b48b6a5df28ee3ef31aa623c9
Nazar Bondarenko <48944609+NazarBondarenkoQ@users.noreply.github.com>
2019-07-07 17:55:44 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,8 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(2, 1, 4, 3, 5, 7, 6));
         Set<Integer> set = new TreeSet<>(new MyComparator());
-        set.addAll(numbers);
+        set.addAll(Arrays.asList(2, 1, 4, 3, 5, 7, 6));
         System.out.print(set + " ");
     }
 }
