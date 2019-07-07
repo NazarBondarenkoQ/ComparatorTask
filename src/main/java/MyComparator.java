698b172d15db0d75bc5b5ed2631f8876741b3a62
Nazar Bondarenko <48944609+NazarBondarenkoQ@users.noreply.github.com>
2019-07-07 19:42:32 +0300

    Update MyComparator.java
    fixed

--- src/main/java/MyComparator.java
+++ src/main/java/MyComparator.java
@@ -4,14 +4,14 @@ import java.util.Comparator;
 
 public class MyComparator implements Comparator<Integer> {
 
-    @Override
-   public int compare(Integer o1, Integer o2) {
-        if (o1 % 2 == 0 && o2 % 2 == 0) {
-            return o1 - o2;
-        } else if (o1 % 2 == 0) {
+@Override
+    public int compare(Integer a, Integer b) {
+        if (a % 2 == 0 && b % 2 != 0) {
             return -1;
-        } else {
+        } else if (a % 2 != 0 && b % 2 == 0) {
             return 1;
+        } else {
+            return a - b;
         }
     }
 }
